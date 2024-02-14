7d8171df4df2a80acd83af4d89843df17227531c
Zach Grande <zach.grande23@gmail.com>
2024-02-14 15:08:08 -0800

    Return 202 status when job is completed

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -2,10 +2,19 @@ module ProgressJob
   class ProgressController < ActionController::Base
 
     def show
-      @delayed_job = Delayed::Job.find(params[:job_id])
-      return unless @delayed_job.present?
-      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      @delayed_job = Delayed::Job.where(id: params[:job_id]).first
+      if @delayed_job.present?
+        $redis.set("progress_job:#{@delayed_job.id}", true, ex: 30)
+        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      else
+        @job = $redis.get("progress_job:#{@delayed_job.id}")
+        if @job.present?
+          render json: {message: "Job Completed"}, status: 202
+        else
+          render status: 404
+        end
+      end
     end
 
   end
