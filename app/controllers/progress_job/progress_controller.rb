bf06f1dd3230c28ba48dbf4f251e4f565229d934
Zach Grande <zach.grande23@gmail.com>
2024-02-15 10:10:55 -0800

    Removed operation to set key-value pair in redis

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -4,7 +4,6 @@ module ProgressJob
     def show
       @delayed_job = Delayed::Job.where(id: params[:job_id]).first
       if @delayed_job.present?
-        $redis.set("progress_job:#{@delayed_job.id}", true, ex: 30)
         percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
         render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
       else
